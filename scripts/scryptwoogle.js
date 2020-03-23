eccc72e8d23867ea5bab96b482b6cc8c017d7f21
Tiago <tiago.estacado@hotmail.com>
2020-03-23 09:58:33 -0300

    project woogle

---
+++ scripts/scryptwoogle.js
@@ -0,0 +1,17 @@
+let btsearch = function() {
+    document.getElementById('isearch').value;
+}
+let confirm = function() {
+    const x;
+    const r = confirm("Tem certeza que o seu nome é: ").document.getElementById('btsearch');
+    if (r == true) {
+        x = alert("Acesso Liberado");
+    } else {
+        x = alert("Você não tem acesso");
+    }
+}
+
+let btlucky = function() {
+
+
+}
