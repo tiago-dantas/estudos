09ac11146ffd5b830de3b05759d4fb8946aab345
Tiago <tiago.estacado@hotmail.com>
2020-03-24 19:56:56 -0300

    teste

--- CURSO JS/Aula 04/script/ex001.js
+++ CURSO JS/Aula 04/script/ex001.js
@@ -1,3 +1,5 @@
 //window.alert('Minha primeira mensagem');
-/*window.confirm('Está pronto para escrever seu nome?')
-window.prompt('Escreve seu nome então!')*/
+/*
+window.confirm('Está pronto para escrever seu nome?')
+window.prompt('Escreve seu nome então!')
+*/
