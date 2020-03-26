a2d60524070c9aae839cff16e78315200e88becf
Tiago <tiago.estacado@hotmail.com>
2020-03-26 01:20:58 -0300

    divs não funcionando

---
+++ aCURSO JS/script/ex003.js
@@ -0,0 +1,42 @@
+// inputs and select
+let lname = document.getElementById(iname).Value;
+let lage = document.getElementById(iage).Value;
+let lgender = document.getElementById(igender).Value;
+let llane = document.getElementById(slane).Value;
+
+
+
+// NÃO ARRUMAR ABAIXO
+
+
+
+// button  //arrumar esse código não está funcionando. Template string deve estar errada.
+// ANDREY, NÃO ARRUMAR. Preciso aprender e vou tentar arrumar.
+
+/*
+let fsubscribe = function() {
+    let lbt = document.getElementById(showsubscribe)
+    lbt.innerHTML(`Thanks ${lname}. 
+    You are ${lgender} and have ${lage}, so your group is GROUP D.
+    You go on ${llane} lane and your position is ${lclass}.
+    Welcome!`)
+}
+*/
+
+// checkbox  //arrumar esse código. Não está funcionando. NÃO USAR RADIO.
+// ANDREY, NÃO ARRUMAR. Preciso aprender e vou tentar arrumar.
+
+/*
+ let lclass = function() {
+lclass = checkgroup
+
+    .input.checkgroup.click.function() {
+    if (lclass(this).is(":checked")) {
+        lclass('input.checkgroup').attr('disabled', true);
+        lclass(this).removeAttr('disabled');
+    } else {
+        lclass('input.checkgroup').removeAttr('disabled');
+    }
+})
+})
+*/
