e8c679500667b8e337a0cf2cb24bca0cbf7d485e
Tiago <tiago.estacado@hotmail.com>
2020-04-05 13:02:45 -0300

    atualizando

--- GOOGLE GLASS/_script/index.js
+++ GOOGLE GLASS/_script/index.js
@@ -1,9 +1,3 @@
-<html>
-    <head>
-        <title>Teste da Lampada</title>
-        <body>
-            <h1>Acenda a Lâmpada</h1>
-            <img src="../_imagens/lamada-apagada.jpg"></img>
-        </body>
-    </head>
-</html>
+let mudafoto = function (foto) {
+    document.getElementById("icone").src = foto
+}
